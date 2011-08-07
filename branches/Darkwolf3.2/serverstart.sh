f3a043f3a4aefc0316d34f87c689c5bb915b1472
special_darkwolf@hotmail.com <special_darkwolf@hotmail.com@494b4778-9eba-11dd-a29e-e3fa11317253>
2011-08-07 18:52:31 +0000

    ant updated to version 1.8.2 . build.xml changed and updated. and the source requires now java7

--- branches/Darkwolf3.2/serverstart.sh
+++ branches/Darkwolf3.2/serverstart.sh
@@ -7,7 +7,7 @@
 while :; do 
   [ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
   [ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log" 
-java -Xms1024m -Xmx1024m -cp l1jen.jar;lib\bonecp-0.7.1.jar;lib\slf4j-api-1.6.1.jar;lib\sfl4j-simple-1.6.1.jar;lib\guava-r09.jar;lib\mysql-connector-java-5.1.17-bin.jar;lib\javolution.jar l1j.server.Server > log/stdout.log 2>&1
+java -Xms1024m -Xmx1024m -cp l1jen.jar;lib\bonecp-0.7.1.jar;lib\mina-core-2.0.4.jar;lib\slf4j-api-1.6.1.jar;lib\sfl4j-simple-1.6.1.jar;lib\guava-r09.jar;lib\mysql-connector-java-5.1.17-bin.jar;lib\javolution.jar l1j.server.Server > log/stdout.log 2>&1
  [ $? -ne 2 ] && break 
 #       /etc/init.d/mysql restart 
          sleep 10 
